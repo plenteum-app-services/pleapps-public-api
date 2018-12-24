6976ef93aab6310c6a7f4a623c68a5253b618cd9
Brandon Lehmann <brandonlehmann@gmail.com>
2018-12-24 14:09:14 -0500

    Initial Commit
    (cherry picked from commit 0bc45672f48522082e7e48a36750497021c57e8f)

---
+++ index.js
@@ -0,0 +1,72 @@
+// Copyright (c) 2018, TurtlePay Developers
+//
+// Please see the included LICENSE file for more information.
+
+'use strict'
+
+const Config = require('./config.json')
+const DatabaseBackend = require('./lib/databaseBackend.js')
+const util = require('util')
+const Compression = require('compression')
+const Helmet = require('helmet')
+const BodyParser = require('body-parser')
+const Express = require('express')
+
+/* Let's set up a standard logger. Sure it looks cheap but it's
+   reliable and won't crash */
+function log (message) {
+  console.log(util.format('%s: %s', (new Date()).toUTCString(), message))
+}
+
+function logHTTPRequest (req, params) {
+  params = params || ''
+  log(util.format('[REQUEST] (%s) %s %s', req.ip, req.path, params))
+}
+
+function logHTTPError (req, message) {
+  message = message || 'Parsing error'
+  log(util.format('[ERROR] (%s) %s: %s', req.ip, req.path, message))
+}
+
+/* Set up our database connection */
+const database = new DatabaseBackend({
+  host: Config.mysql.host,
+  port: Config.mysql.port,
+  username: Config.mysql.username,
+  password: Config.mysql.password,
+  database: Config.mysql.database,
+  connectionLimit: Config.mysql.connectionLimit
+})
+
+log('Connected to database backend at ' + database.host + ':' + database.port)
+
+const app = Express()
+
+/* Automatically decode JSON input from client requests */
+app.use(BodyParser.json())
+
+/* Set up a few of our headers to make this API more functional */
+app.use((req, res, next) => {
+  res.header('X-Requested-With', '*')
+  res.header('Access-Control-Allow-Origin', Config.corsHeader)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.header('Cache-Control', 'max-age=30, public')
+  next()
+})
+
+/* Set up our system to use Helmet */
+app.use(Helmet())
+
+/* Last but certainly not least, enable compression because we're going to need it */
+app.use(Compression())
+
+/* This is our catch all to return a 404-error */
+app.all('*', (req, res) => {
+  logHTTPError(req, 'Requested URL not Found (404)')
+  return res.status(404).send()
+})
+
+app.listen(Config.httpPort, Config.bindIp, () => {
+  log('HTTP server started on ' + Config.bindIp + ':' + Config.httpPort)
+})

61b1042e389318c698917fae0c58b205974929ed
Brandon Lehmann <brandonlehmann@gmail.com>
2019-04-07 18:45:57 -0400

    Resolves: https://github.com/TurtlePay/turtlepay-public-api/issues/1 Replaces: https://github.com/TurtlePay/turtlepay-public-api/pull/4
    Special credit to @mrrovot for the work put in on the PR that this incorporates

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-// Copyright (c) 2018, TurtlePay Developers
+// Copyright (c) 2018-2019, TurtlePay Developers
 //
 // Please see the included LICENSE file for more information.
 
 'use strict'
 
+const AES = require('./lib/aes.js')
+const BodyParser = require('body-parser')
+const Compression = require('compression')
 const Config = require('./config.json')
 const DatabaseBackend = require('./lib/databaseBackend.js')
-const TurtleCoinUtils = require('turtlecoin-utils').CryptoNote
-const cryptoUtils = new TurtleCoinUtils()
-const util = require('util')
-const Compression = require('compression')
-const Helmet = require('helmet')
-const BodyParser = require('body-parser')
 const Express = require('express')
-const walletQueue = 'request.wallet'
+const Helmet = require('helmet')
 const RabbitMQ = require('amqplib')
+const TurtleCoinUtils = require('turtlecoin-utils').CryptoNote
+const util = require('util')
 const UUID = require('uuid/v4')
+
+const cryptoUtils = new TurtleCoinUtils()
+const walletQueue = 'request.wallet'
 
 const publicRabbitHost = process.env.RABBIT_PUBLIC_SERVER || 'localhost'
 const publicRabbitUsername = process.env.RABBIT_PUBLIC_USERNAME || ''
 const publicRabbitPassword = process.env.RABBIT_PUBLIC_PASSWORD || ''
+const buttonContainerPassword = process.env.BUTTON_CONTAINER_PASSWORD || ''
+
+const crypto = new AES({ password: buttonContainerPassword })
 
 /* Helps us to build the RabbitMQ connection string */
 function buildConnectionString (host, username, password) {
@@ -116,9 +121,7 @@ app.use(Helmet())
 /* Last but certainly not least, enable compression because we're going to need it */
 app.use(Compression())
 
-/* This is the meat and potatoes entry method for the public API
-   aka, submitting a new request for funds to the processing engine */
-app.post('/v1/new', async function (req, res) {
+function validateNewRequest (req) {
   const atomicAmount = toNumber(req.body.amount)
   const callback = req.body.callback || false
   const address = req.body.address || false
@@ -126,36 +129,36 @@ app.post('/v1/new', async function (req, res) {
   const callerData = req.body.userDefined || {}
   const confirmations = toNumber(req.body.confirmations)
 
-  if (!atomicAmount) {
-    logHTTPError(req, 'Invalid amount supplied')
-    return res.status(400).send()
+  /* Validate that the caller has specified an amount for the request */
+  if (!atomicAmount || atomicAmount === 0 || atomicAmount < 0) {
+    return { error: {
+      message: 'Invalid Amount Supplied',
+      retCode: 400
+    } }
   }
 
   const amount = (atomicAmount / Math.pow(10, Config.coinDecimals))
-  var cancelTimer
 
   /* Validate that the caller has supplied a valid CryptoNote address
      for us to send funds to */
   try {
     cryptoUtils.decodeAddress(address)
   } catch (e) {
-    logHTTPError(req, 'Invalid address supplied')
-    return res.status(400).send()
+    return { error: {
+      message: 'Invalid address supplied',
+      retCode: 400
+    } }
   }
 
   /* Verify that the caller supplied us with an acceptable callback
      URL that we'll post back to later */
   if (callback) {
     if (callback.substring(0, 4).toLowerCase() !== 'http') {
-      logHTTPError(req, 'Invalid callback URL supplied')
-      return res.status(400).send()
+      return { error: {
+        message: 'Invalid callback URL supplied',
+        retCode: 400
+      } }
     }
-  }
-
-  /* Verify that the caller has supplied a valid amount to request */
-  if (!atomicAmount || atomicAmount === 0 || atomicAmount < 0) {
-    logHTTPError(req, 'Invalid amount requested')
-    return res.status(400).send()
   }
 
   /* If the caller has supplied the number of confirmations that they
@@ -166,8 +169,10 @@ app.post('/v1/new', async function (req, res) {
     /* If the caller requested 0 or less or more confirmations than we
        allow, we're going to reject their request */
     if (confirmations < 0 || confirmations > Config.maximumConfirmations) {
-      logHTTPError(req, 'Invalid confirmations requested')
-      return res.status(400).send()
+      return { error: {
+        message: 'Invalid confirmations requested',
+        retCode: 400
+      } }
     }
     requestConfirmations = confirmations
   } else {
@@ -176,75 +181,196 @@ app.post('/v1/new', async function (req, res) {
     requestConfirmations = Config.defaultConfirmations
   }
 
-  try {
+  return {
+    atomicAmount,
+    amount,
+    callback,
+    address,
+    name,
+    callerData,
+    requestConfirmations
+  }
+}
+
+function processNewRequest (validationResult) {
+  return new Promise((resolve, reject) => {
+    var cancelTimer
+
+    try {
     /* Generate a random request ID for use by the RPC client */
-    const requestId = UUID().toString().replace(/-/g, '')
+      const requestId = UUID().toString().replace(/-/g, '')
 
-    /* Assemble the data we're passing to the backend workers */
-    const walletRequest = {
-      amount: atomicAmount,
-      address: address,
-      confirmations: requestConfirmations,
-      callback: callback,
-      callerData: callerData
-    }
+      /* Assemble the data we're passing to the backend workers */
+      const walletRequest = {
+        amount: validationResult.atomicAmount,
+        address: validationResult.address,
+        confirmations: validationResult.requestConfirmations,
+        callback: validationResult.callback,
+        callerData: validationResult.callerData
+      }
 
-    /* Here, we set up our worker side of the queue to grab the replyQueue
+      /* Here, we set up our worker side of the queue to grab the replyQueue
        from the backend workers so we can spit the results back to the client */
-    channel.consume(replyQueue.queue, (message) => {
+      channel.consume(replyQueue.queue, (message) => {
       /* If we received a valid message and it matches our request let's tell the caller */
-      if (message !== null && message.properties.correlationId === requestId) {
-        var workerResponse = JSON.parse(message.content.toString())
-        var sendToAddress = workerResponse.address
+        if (message !== null && message.properties.correlationId === requestId) {
+          var workerResponse = JSON.parse(message.content.toString())
+          var sendToAddress = workerResponse.address
 
-        /* Acknowledge to RabbitMQ that we've received the request and we're handling it */
-        channel.ack(message)
+          /* Acknowledge to RabbitMQ that we've received the request and we're handling it */
+          channel.ack(message)
 
-        /* We received a response so we don't need this timer anymore */
-        if (cancelTimer !== null) {
-          clearTimeout(cancelTimer)
-        }
+          /* We received a response so we don't need this timer anymore */
+          if (cancelTimer !== null) {
+            clearTimeout(cancelTimer)
+          }
 
-        /* Log the request and spit the response back to the caller */
-        logHTTPRequest(req, JSON.stringify(walletRequest))
-        return res.json({
-          sendToAddress: sendToAddress,
-          atomicAmount: atomicAmount,
-          amount: amount,
-          userDefined: callerData,
-          startHeight: workerResponse.scanHeight,
-          endHeight: workerResponse.maxHeight,
-          confirmations: requestConfirmations,
-          callbackPublicKey: workerResponse.publicKey,
-          qrCode: 'https://chart.googleapis.com/chart?cht=qr&chs=256x256&chl=' + Config.coinUri + '://' + sendToAddress + '?amount=' + atomicAmount + ((name) ? '&name=' + encodeURIComponent(name) : '')
-
-        })
-      } else if (message !== null) {
+          return resolve({
+            logMessage: JSON.stringify(walletRequest),
+            response: {
+              sendToAddress: sendToAddress,
+              atomicAmount: validationResult.atomicAmount,
+              amount: validationResult.amount,
+              userDefined: validationResult.callerData,
+              startHeight: workerResponse.scanHeight,
+              endHeight: workerResponse.maxHeight,
+              confirmations: validationResult.requestConfirmations,
+              callbackPublicKey: workerResponse.publicKey,
+              qrCode: 'https://chart.googleapis.com/chart?cht=qr&chs=256x256&chl=' + Config.coinUri + '://' + sendToAddress + '?amount=' + validationResult.atomicAmount + ((validationResult.name) ? '&name=' + encodeURIComponent(validationResult.name) : '')
+            }
+          })
+        } else if (message !== null) {
         /* There was a message, but it wasn't for us. Let it go back
            in the queue for someone else to handle */
-        channel.nack(message)
-      }
-    })
+          channel.nack(message)
+        }
+      })
 
-    /* Send the request to create a wallet to the queue for processing
+      /* Send the request to create a wallet to the queue for processing
        by the backend workers and give it a time limit of 2s */
-    channel.sendToQueue(walletQueue, Buffer.from(JSON.stringify(walletRequest)), {
-      correlationId: requestId,
-      replyTo: replyQueue.queue,
-      expiration: 2000
-    })
+      channel.sendToQueue(walletQueue, Buffer.from(JSON.stringify(walletRequest)), {
+        correlationId: requestId,
+        replyTo: replyQueue.queue,
+        expiration: 2000
+      })
 
-    /* Define a timer that if we don't get a response back in 2.5s or less
+      /* Define a timer that if we don't get a response back in 5s or less
        that we need to consider the request failed and let the caller know
        that something went wrong */
-    cancelTimer = setTimeout(() => {
-      logHTTPError(req, 'RPC request timed out')
-      return res.status(500).send()
-    }, 5000)
-  } catch (e) {
-    logHTTPError(req, e.toString())
-    return res.status(500).send()
+      cancelTimer = setTimeout(() => {
+        return resolve({
+          error: {
+            message: 'RPC request time out',
+            retCode: 500
+          }
+        })
+      }, 5000)
+    } catch (e) {
+      return resolve({
+        error: {
+          message: e.toString(),
+          retCode: 500
+        }
+      })
+    }
+  })
+}
+
+/* This is the meat and potatoes entry method for the public API
+   aka, submitting a new request for funds to the processing engine */
+app.post('/v1/new', (req, res) => {
+  /* Validate that our request has no errors in the data that the
+     caller supplied, or let them know we won't be proceeding */
+  const validationResult = validateNewRequest(req)
+  if (validationResult.error) {
+    logHTTPError(req, validationResult.error.message)
+    return res.status(validationResult.error.retCode).send()
   }
+
+  /* Try to process the request using the backend workers */
+  processNewRequest(validationResult).then((result) => {
+    if (result.error) {
+      logHTTPError(req, result.error.message)
+      return res.status(result.error.retCode).send()
+    }
+
+    logHTTPRequest(req, result.logMessage)
+    return res.json(result.response)
+  })
+})
+
+app.post('/v1/button', (req, res) => {
+  const encryptedButtonPayload = req.body.buttonPayload || false
+
+  if (!encryptedButtonPayload) {
+    logHTTPError(req, 'No button payload provided')
+    return res.status(400).send()
+  }
+
+  try {
+    /* Try to decrypt the data from the button payload */
+    const validationResult = crypto.decrypt(encryptedButtonPayload)
+
+    /* Try to process the request using the backend workers */
+    processNewRequest(validationResult).then((result) => {
+      if (result.error) {
+        logHTTPError(req, result.error.message)
+        return res.status(result.error.retCode).send()
+      }
+
+      logHTTPRequest(req, result.logMessage)
+      return res.json(result.response)
+    })
+  } catch (e) {
+    logHTTPError(req, 'Invalid button payload provided')
+    return res.status(400).send()
+  }
+})
+
+app.get('/v1/button/:buttonPayload', (req, res) => {
+  const encryptedButtonPayload = req.params.buttonPayload || false
+
+  if (!encryptedButtonPayload) {
+    logHTTPError(req, 'No button payload provided')
+    return res.status(400).send()
+  }
+
+  try {
+    /* Try to decrypt the data from the button payload */
+    const validationResult = crypto.decrypt(encryptedButtonPayload)
+
+    /* Try to process the request using the backend workers */
+    processNewRequest(validationResult).then((result) => {
+      if (result.error) {
+        logHTTPError(req, result.error.message)
+        return res.status(result.error.retCode).send()
+      }
+
+      logHTTPRequest(req, result.logMessage)
+      return res.json(result.response)
+    })
+  } catch (e) {
+    logHTTPError(req, 'Invalid button payload provided')
+    return res.status(400).send()
+  }
+})
+
+app.post('/v1/button/new', (req, res) => {
+  /* Validate that our request has no errors in the data that the
+     caller supplied, or let them know we won't be proceeding */
+  const validationResult = validateNewRequest(req)
+  if (validationResult.error) {
+    logHTTPError(req, validationResult.error.message)
+    return res.status(validationResult.error.retCode).send()
+  }
+
+  const encryptedButtonPayload = crypto.encrypt(validationResult)
+
+  logHTTPRequest(req, JSON.stringify(validationResult))
+
+  return res.json({
+    buttonPayload: encryptedButtonPayload
+  })
 })
 
 /* Response to options requests for preflights */
